56841b6d882192ac7f574a3f2e564ce97d140fd8
Carl Pearson <pearson@illinois.edu>
2017-10-30 18:46:56 -0500

    Remove wrapper

--- filter_mxnet.py
+++
@@ -1,9 +0,0 @@
-import fileinput
-import sys
-
-for line in fileinput.input():
-    if line.startswith("====ECE408FORWARD===="):
-        pass
-    else:
-        print line,
-    sys.stdout.flush()
--- wrapper.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-$@ | python /filter_mxnet.py
